7be34c34e8e8671dc593bdaedd1c16e1fd9d8e2c
ryomasenbra <ryomasenbra@gmail.com>
2025-02-27 08:12:08 +0000

    update_contents

--- app/dashboard/disconbenefits/page.tsx
+++ app/dashboard/disconbenefits/page.tsx
@@ -4,7 +4,18 @@ export default function Page() {
     return (
         <div>
             <AddCalculate /> {/* AddCalculateコンポーネントを表示 */}
+            <div style={{ marginTop: '40px' }}>
+                <h1>総括安全衛生管理者</h1>
+                <img src="/総括安全衛生管理者.png"  width={300} height={200} />
+            </div>
+            <div style={{ marginTop: '40px' }}>
+                <h1>安全管理者(専属must)</h1>
+                <img src="/安全管理者.png"  width={300} height={200} />
+                <h2>専任要件</h2>
+                <img src="/安全管理者_専任.png"  width={300} height={200} />
+            </div>
         </div>
+
     );
   }
   
