5c5ac8e8f82651ef63b783cb9c9d18364e6ea43a
ryomasenbra <ryomasenbra@gmail.com>
2025-02-28 08:05:28 +0000

    update_contents

--- app/dashboard/disconbenefits/page.tsx
+++ app/dashboard/disconbenefits/page.tsx
@@ -10,9 +10,24 @@ export default function Page() {
             </div>
             <div style={{ marginTop: '40px' }}>
                 <h1>安全管理者(専属must)</h1>
+                <ul>
+                    <li>厚生労働大臣の定める研修 + 実務</li>
+                    <li>大卒+2年 or 高卒+4年</li>
+                    <li>巡視：頻度なし</li>
+                </ul>
                 <img src="/安全管理者.png"  width={300} height={200} />
                 <h2>専任要件</h2>
                 <img src="/安全管理者_専任.png"  width={300} height={200} />
+            </div>
+            <div style={{ marginTop: '40px' }}>
+                <h1>衛生管理者(専属must)</h1>
+                <ul>
+                    <li>都道府県労働局長の免許(1種・2種)</li>
+                    <li>保健師、薬剤師</li>
+                    <li>衛生工学衛生管理者</li>
+                    <li>巡視：週１回</li>
+                </ul>
+                <img src="/衛生管理者.png"  width={300} height={200} />
             </div>
         </div>
 
