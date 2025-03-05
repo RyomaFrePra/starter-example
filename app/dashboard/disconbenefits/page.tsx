1299a7f986194bc562afc8d9dd6902d78df76e65
ryomasenbra <ryomasenbra@gmail.com>
2025-03-05 08:30:48 +0000

    update_contents

--- app/dashboard/disconbenefits/page.tsx
+++ app/dashboard/disconbenefits/page.tsx
@@ -29,6 +29,32 @@ export default function Page() {
                 </ul>
                 <img src="/衛生管理者.png"  width={300} height={200} />
             </div>
+            <div style={{ marginTop: '40px' }}>
+                <h1>労災と雇用保険</h1>
+                <img src="/労災と雇用保険_対象比較.png"  width={300} height={200} />
+            </div>
+            <div style={{ marginTop: '40px' }}>
+                <h1>労災と雇用保険の二元適用</h1>
+                <ul>
+                    <li>都道府県及び市町村の行う事業</li>
+                    <li>港湾運送</li>
+                    <li>農林・畜産・養蚕・水産</li>
+                    <li>建設</li>
+                </ul>
+            </div>
+            <div style={{ marginTop: '40px' }}>
+                <h1>労災保険法で賃金に該当しないもの</h1>
+                <ul>
+                    <li>休業補償</li>
+                    <li>業務外のけがに対する恩恵的な休業手当</li>
+                    <li>結婚祝金、死亡弔慰金、災害見舞金など(支給要件が明確でも該当しない)</li>
+                    <li>退職金・退職一時金(賃金の一部に含める場合は該当する)</li>
+                </ul>
+            </div>
+            <div style={{ marginTop: '40px' }}>
+                <h1>労災保険法　賃金総額の特例</h1>
+                <img src="/労災保険_賃金総額の特例.png"  width={300} height={200} />
+            </div>
         </div>
 
     );
