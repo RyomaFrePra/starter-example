b73a1bfd8fd22d9cce8fdea5e0fa6f7c5da2df94
ryomasenbra <ryomasenbra@gmail.com>
2025-04-11 07:56:30 +0000

    update_contents

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -40,6 +40,28 @@ export default function Page() {
                 <h1>年金の内払システム</h1>
                 <img src="/年金の内払システム.png"  width={300} height={400} />
       </div>
+      <div style={{ marginTop: '40px' }}>
+                <h1>減額・費用徴収</h1>
+                <h2>本人</h2>
+                <ul>
+                  <li>犯罪・過失：30%</li>
+                  <li>療養の支持違反：10/365</li>
+                  <li>遺族・介護の給付：減額しない</li>
+                </ul>
+                <h2>事業主</h2>
+                <ul>
+                  <li>届け出を出していない：40％</li>
+                  <li>過失の業務災害：30%</li>
+                  <li>過失の通勤災害：無罪</li>
+                  <li>療養・介護・二次診断：無罪</li>
+                </ul>
+      </div>
+      <div style={{ marginTop: '40px' }}>
+                <h1>特別支給金</h1>
+                <img src="/労災の特別支給金_page-0001.jpg"  width={300} height={800} />
+                <img src="/労災の特別支給金_page-0002.jpg"  width={300} height={800} />
+                <img src="/労災の特別支給金_page-0003.jpg"  width={300} height={800} />
+      </div>
     </div>
     )
   }
